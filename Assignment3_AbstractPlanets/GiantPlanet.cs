4e7453822b97727f5085eb75a4b4afdf91a37af9
kristinamaniacup <kc_maniacup@hotmail.com>
2015-02-13 14:48:54 -0800

    Created abstract Planet class, started giantPlanet class

---
+++ Assignment3_AbstractPlanets/GiantPlanet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_AbstractPlanets
+{
+    class GiantPlanet : Planet
+    {
+        //PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++
+        private string _type;
+
+
+        //CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public GiantPlanet(string type, string name, double diameter, double mass)
+            : base(name, diameter, mass)
+        {
+            this._type = type;
+        }
+
+        //PUBLIC METHOD(S) +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public bool HasMoons()
+        {
+            
+            if(MoonCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool HasRings()
+        {
+            if (RingCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+}
---
+++ Assignment3_AbstractPlanets/Planet.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3_AbstractPlanets
+{
+    abstract class Planet
+    {
+        //PRIVATE INSTANCE VARIABLES ++++++++++++++++++++++++++++++++++++++++++++++
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+
+        //PUBLIC PROPERTIES +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public double Diameter
+        {
+            get
+            {
+                return this._diameter;
+            }
+        }
+
+        public double Mass
+        {
+            get
+            {
+                return this._mass;
+            }
+        }
+
+        public int MoonCount
+        {
+            get
+            {
+                return this._moonCount;
+            }
+            set
+            {
+                this._moonCount = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+        }
+
+        public double OrbitalPeriod
+        {
+            get
+            {
+                return this._orbitalPeriod;
+            }
+            set
+            {
+                this._orbitalPeriod = value;
+            }
+        }
+
+        public int RingCount
+        {
+            get
+            {
+                return this._ringCount;
+            }
+            set
+            {
+                this._ringCount = value;
+            }
+        }
+
+        public double RotationPeriod
+        {
+            get
+            {
+                return this._rotationPeriod;
+            }
+            set
+            {
+                this._rotationPeriod = value;
+            }
+        }
+
+        //CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
+
+        //PUBLIC METHOD(S) +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public override string ToString()
+        {
+            return this.Name + " is the planet, " + this.Diameter + " is the diameter and " + this.Mass + " is it's mass.";
+        }
+    }
+}
